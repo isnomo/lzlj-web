1ae0c2eab431243b26e8c6604c6fa293740c4545
nomo <nomo1717@163.com>
2018-06-05 18:13:15 +0800

    增加js

--- js/lzlj.js
+++ js/lzlj.js
@@ -1,4 +1,41 @@
 (function () {
+  let winWidth = document.body.clientWidth
+  let winHeight = window.innerHeight
+  if (winWidth > winHeight) {
+    $('.rotate-hint').css({'display' : 'block'})
+  }else{
+    $('.rotate-hint').css({'display' : 'none'})
+  }
+
+  // init
+  var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.next-page',
+      // prevEl: '.swiper-button-prev',
+    },
+    speed: 500,
+    // effect : 'cube',
+    allowTouchMove: false,
+    // hashNavigation: {
+    //   watchState: true,
+    // }
+  });
+
+  // 选择后显示下一页按钮
+  $('input[type=radio]').on('click',function(event){
+    let pageNum = $(this).attr('name')
+    let thisBtn = $(this).parents('.select-main__option').next()
+    thisBtn.find('button').attr('disabled',false)
+  })
+
+  $('#complete-button').on('click',function(event){
+    let form = document.getElementById("userForm");
+    let selectData = new FormData(form)
+    console.log(selectData)
+    console.log(selectData.get('page1'))
+  })
+
+  
   $(window).load(() => {
   
   })
