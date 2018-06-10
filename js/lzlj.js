4119c0ccb49a423d91be96e8a6f89b401bb88406
nomo <nomo1717@163.com>
2018-06-10 16:37:20 +0800

    2018年6月10日16:37:19

--- js/lzlj.js
+++ js/lzlj.js
@@ -68,7 +68,7 @@
 
   // alert(winWidth + 'x' + winHeight)
   // 兼容iphoneX
-  if(winHeight / winWidth > 1.8){
+  if (winHeight / winWidth > 1.8) {
     $('.lzlj-main .bg-wrapper .page-select').css({ 'padding': '0 5% 20%' })
     // alert('长手机')
   }
@@ -97,10 +97,11 @@
           setTimeout(() => {
             $('.results .results-bottom .results-bottom__btn').addClass('show')
           }, 800)
-          $('#click-img').click(function(){
-            html2canvas(document.querySelector("#download-img")).then(canvas => {
-              document.querySelector("#show-img").appendChild(canvas)
-            })
+          $('#click-img').click(function () {
+            // html2canvas(document.querySelector("#download-img")).then(canvas => {
+            //   document.querySelector("#show-canvas").appendChild(canvas)
+            // })
+            convert2canvas()
           })
         }
       },
@@ -135,9 +136,9 @@
       }
     })
     console.log(resultsData)
-    for(let i = 0; i < titleJson.length; i++){
+    for (let i = 0; i < titleJson.length; i++) {
       let itemTitle = titleJson[i]
-      if(itemTitle.page4 == page4Val && itemTitle.page8 == page8Val){
+      if (itemTitle.page4 == page4Val && itemTitle.page8 == page8Val) {
         console.log(itemTitle)
         $('.results .results-top .tags-left').children('div:first-child').html(itemTitle.tags[0])
         $('.results .results-top .tags-left').children('div:last-child').html(itemTitle.tags[2])
@@ -161,4 +162,47 @@
     let toTop = $(this).scrollTop()
 
   })
+
+  function convert2canvas() {
+
+    var cntElem = $('#download-img')[0];
+
+    var shareContent = cntElem;//需要截图的包裹的（原生的）DOM 对象
+    var width = shareContent.offsetWidth; //获取dom 宽度
+    var height = shareContent.offsetHeight; //获取dom 高度
+    var canvas = document.createElement("canvas"); //创建一个canvas节点
+    var scale = 2; //定义任意放大倍数 支持小数
+    canvas.width = width * scale; //定义canvas 宽度 * 缩放
+    canvas.height = height * scale; //定义canvas高度 *缩放
+    canvas.getContext("2d").scale(scale, scale); //获取context,设置scale 
+    var opts = {
+      scale: scale, // 添加的scale 参数
+      canvas: canvas, //自定义 canvas
+      // logging: true, //日志开关，便于查看html2canvas的内部执行流程
+      width: width, //dom 原始宽度
+      height: height,
+      useCORS: true // 【重要】开启跨域配置
+    };
+
+    html2canvas(shareContent, opts).then(function (canvas) {
+
+      var context = canvas.getContext('2d');
+      // 【重要】关闭抗锯齿
+      context.mozImageSmoothingEnabled = false;
+      context.webkitImageSmoothingEnabled = false;
+      context.msImageSmoothingEnabled = false;
+      context.imageSmoothingEnabled = false;
+
+      // 【重要】默认转化的格式为png,也可设置为其他格式
+      var img = Canvas2Image.convertToPNG(canvas, canvas.width, canvas.height);
+
+      document.querySelector("#show-img").appendChild(img);
+      $(img).css({
+        "width": canvas.width / 2 + "px",
+        "height": canvas.height / 2 + "px",
+      }).addClass('f-full');
+
+    });
+  }
+
 })()
